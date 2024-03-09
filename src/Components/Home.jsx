e3fa69b6915fc1397f2fb4c0ca5167afc4c5f851
dagmfre <dagmfre@gmail.com>
2024-03-09 21:24:58 +0300

    Build the structure of the navbar

---
+++ src/Components/Home.jsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <div className="home">
+      <div className="home-cont">
+        <div className="navbar">
+          <div>
+            <h1>Redux Toolkit</h1>
+            <p>Products</p>
+          </div>
+          <div>
+            <i class="fa-solid fa-cart-shopping"></i>
+            <p>1</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
